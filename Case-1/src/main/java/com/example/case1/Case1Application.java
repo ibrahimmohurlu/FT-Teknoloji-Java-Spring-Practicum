af173e118686ef0eedd8b0b33082302ac5091ac2
ibrahimmohurlu <ibrahimmohurlu@gmail.com>
2022-08-25 14:11:50 +0300

    Case-1 created.

---
+++ Case-1/src/main/java/com/example/case1/Case1Application.java
@@ -0,0 +1,13 @@
+package com.example.case1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Case1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Case1Application.class, args);
+    }
+
+}
---
+++ Case-1/src/test/java/com/example/case1/Case1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.case1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Case1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
