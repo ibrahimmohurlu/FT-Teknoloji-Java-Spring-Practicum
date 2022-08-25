8aafb82ee8b348fdeb8523c832a494fa49a42b86
ibrahimmohurlu <ibrahimmohurlu@gmail.com>
2022-08-25 15:50:19 +0300

    User entity class added.

---
+++ Case-1/src/main/java/com/example/case1/User/User.java
@@ -0,0 +1,116 @@
+package com.example.case1.User;
+
+import javax.persistence.*;
+
+
+@Entity(name = "`User`")
+@Table
+public class User {
+
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    @Column(name = "id",
+            updatable = false)
+    private Long id;
+
+    @Column(
+            name = "first_name",
+            nullable = false,
+            length = 50
+
+    )
+    private String firstName;
+
+    @Column(
+            name = "last_name",
+            nullable = false,
+            length = 50
+    )
+    private String lastName;
+
+    @Column(
+            name = "email",
+            nullable = false,
+            length = 50
+    )
+    private String email;
+
+    @Column(
+            name = "phone_number",
+            nullable = false,
+            length = 15
+    )
+    private String phoneNumber;
+
+    public User(Long id, String firstName, String lastName, String email, String phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public User() {
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
