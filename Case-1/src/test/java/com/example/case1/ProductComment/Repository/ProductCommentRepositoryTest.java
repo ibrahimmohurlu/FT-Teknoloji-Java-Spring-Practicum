3098700b226b14d1672a134fd3132ff0e3099099
ibrahimmohurlu <ibrahimmohurlu@gmail.com>
2022-09-05 15:37:24 +0300

    ProductCommentRepository tests updated.

--- Case-1/src/test/java/com/example/case1/ProductComment/Repository/ProductCommentRepositoryTest.java
+++ Case-1/src/test/java/com/example/case1/ProductComment/Repository/ProductCommentRepositoryTest.java
@@ -6,6 +6,7 @@ import com.example.case1.ProductComment.Entity.ProductComment;
 import com.example.case1.User.Entity.User;
 import com.example.case1.User.Repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -16,6 +17,9 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 class ProductCommentRepositoryTest {
@@ -25,66 +29,72 @@ class ProductCommentRepositoryTest {
     private ProductCommentRepository productCommentRepository;
     @Autowired
     private UserRepository userRepository;
-    //Repositories tested on a pre-filled database
+
+    // product and user objects will be used to create product comment object.
+    private Product product;
+    private User user;
+    private ProductComment comment;
+
+    @BeforeEach
+    void setUp() {
+        //creating product and user objects and saving to database.
+        //These objects must be present in the database to create productComment.
+        product = new Product();
+        product.setName("product");
+        product.setPrice(11.99);
+        product.setExpDate(LocalDate.of(2020, 5, 23));
+
+        user = new User();
+        user.setFirstName("ibrahim");
+        user.setLastName("mohurlu");
+        user.setEmail("him@gmail.com");
+        user.setPhoneNumber("999-123-1234");
+
+        comment = new ProductComment();
+        comment.setComment("This is an awesome product!");
+        comment.setCommentDate(LocalDate.now());
+        comment.setProduct(product);
+        comment.setUser(user);
+
+        //saving objects to database.
+        userRepository.save(user);
+        productRepository.save(product);
+        productCommentRepository.save(comment);
+
+    }
+
     @Test
     void findByProduct() {
-        //Choose random product based on ID
-        //then try to retrieve comments based on that Product object.
-        Optional<Product> foundProduct = productRepository.findById(3L);
-        if (foundProduct.isPresent()) {
-            List<ProductComment> comments = productCommentRepository.findByProduct(foundProduct.get());
-            Assertions.assertNotEquals(comments.size(), 0);
-            for (ProductComment c : comments) {
-                System.out.println(c.toString());
-            }
-        }
+        List<ProductComment> foundComments = productCommentRepository.findByProduct(product);
+        assertThat(foundComments.size() > 0);
     }
 
     @Test
     void findByUser() {
-        //Choose a random User by ID
-        //then try to retrieve comments based on that user object
-        Optional<User> foundUser = userRepository.findById(3L);
-        if (foundUser.isPresent()) {
-            List<ProductComment> comments = productCommentRepository.findByUser(foundUser.get());
-            Assertions.assertNotEquals(comments.size(), 0);
-            for (ProductComment c : comments) {
-                System.out.println(c.toString());
-            }
-        }
+        List<ProductComment> foundComments = productCommentRepository.findByUser(user);
+        assertThat(foundComments.size()>0);
     }
 
     @Test
     void findAllByProductAndCommentDateBetween() {
-        //pick random product then try to retrieve comments between dates
-        Optional<Product> foundProduct = productRepository.findById(3L);
-
-        if (foundProduct.isPresent()) {
-            List<ProductComment> comments = productCommentRepository.findAllByProductAndCommentDateBetween(
-                    foundProduct.get(),
-                    LocalDate.of(2021,1,1),
-                    LocalDate.of(2022,12,30));
-            Assertions.assertNotEquals(comments.size(), 0);
-            for (ProductComment c : comments) {
-                System.out.println(c.toString());
-            }
-        }
+        //testing between 3 months before and after current date.
+        List<ProductComment> foundComments = productCommentRepository.findAllByProductAndCommentDateBetween(
+                product,
+                LocalDate.now().minusMonths(3L),
+                LocalDate.now().plusMonths(3L)
+        );
+        assertThat(foundComments.size()>0);
     }
 
     @Test
     void findAllByUserAndCommentDateBetween() {
-        //pick random user then try to retrieve comments between dates
-        Optional<User> foundUser = userRepository.findById(3L);
-        if (foundUser.isPresent()) {
-            List<ProductComment> comments = productCommentRepository.findAllByUserAndCommentDateBetween(
-                    foundUser.get(),
-                    LocalDate.of(2021,1,1),
-                    LocalDate.of(2022,12,30));
-            Assertions.assertNotEquals(comments.size(), 0);
-            for (ProductComment c : comments) {
-                System.out.println(c.toString());
-            }
-        }
+        //testing between 3 months before and after current date.
+        List<ProductComment> foundComments = productCommentRepository.findAllByUserAndCommentDateBetween(
+                user,
+                LocalDate.now().minusMonths(3L),
+                LocalDate.now().plusMonths(3L)
+        );
+        assertThat(foundComments.size()>0);
 
     }
 }
