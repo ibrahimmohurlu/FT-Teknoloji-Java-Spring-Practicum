8e6c6b61b5d93e1573bf098082020777b6890d62
ibrahimmohurlu <ibrahimmohurlu@gmail.com>
2022-09-05 23:45:05 +0300

    ProductService and UserService integration tests implemented and test results added as screenshot.

--- Case-1/src/test/java/com/example/case1/Product/Service/ProductServiceTest.java
+++ Case-1/src/test/java/com/example/case1/Product/Service/ProductServiceTest.java
@@ -1,56 +1,102 @@
 package com.example.case1.Product.Service;
 
 import com.example.case1.Product.Entity.Product;
+import com.example.case1.Product.Repository.ProductRepository;
 import com.example.case1.ProductComment.Entity.ProductComment;
-import org.junit.jupiter.api.Assertions;
+import com.example.case1.ProductComment.Repository.ProductCommentRepository;
+import com.example.case1.User.Entity.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 class ProductServiceTest {
-    @Autowired
+    private final Product returnProduct = new Product();
+    @Mock
+    private ProductRepository productRepository;
+    @Mock
+    private ProductCommentRepository productCommentRepository;
+
     private ProductService productService;
 
-    @Test
-    void getCommentsByProductId() {
-        List<ProductComment> commentList = productService.getCommentsByProductId(3L);
-        Assertions.assertNotEquals(commentList.size(), 0);
-        for (ProductComment c : commentList) {
-            System.out.println(c.toString());
-        }
+    @BeforeEach
+    void setUp() {
+        returnProduct.setId(11L);
+        returnProduct.setName("Expensive Mock Product!");
+        returnProduct.setPrice(469.89);
+        returnProduct.setExpDate(LocalDate.now().plusMonths(3L));
+
+        User mockUser = new User();
+        mockUser.setFirstName("ibrahim");
+        mockUser.setLastName("mohurlu");
+        mockUser.setEmail("him@mail.com");
+        mockUser.setPhoneNumber("123-9876-123");
+        mockUser.setId(5L);
+
+        ProductComment returnComment = new ProductComment();
+        returnComment.setId(2L);
+        returnComment.setComment("Mock product is very expensive im not buying!");
+        returnComment.setCommentDate(LocalDate.now());
+        returnComment.setProduct(returnProduct);
+        returnComment.setUser(mockUser);
+
+
+        Mockito.when(productRepository.findById(11L)).thenReturn(Optional.of(returnProduct));
+        Mockito.when(productCommentRepository.findByProduct(returnProduct)).thenReturn(List.of(returnComment));
+
+        productService = new ProductService(productRepository, productCommentRepository);
+
     }
 
     @Test
-    void getCommentsByProductIdBetweenDate() {
-        List<ProductComment> commentList = productService.getCommentsByProductIdBetweenDate(
-                8L,
-                "2021-01-01",
-                LocalDate.now().toString());
-        Assertions.assertNotEquals(commentList.size(), 0);
-        for (ProductComment c : commentList) {
-            System.out.println(c.toString());
-        }
+    void canGetCommentsByProductId() {
+        productService.getCommentsByProductId(11L);
+        //verify method calls in productService
+        verify(productRepository).findById(11L);
+        verify(productCommentRepository).findByProduct(returnProduct);
+
     }
 
     @Test
-    void getExpiredProducts() {
-        List<Product> products = productService.getExpiredProducts();
-        Assertions.assertNotEquals(products.size(), 0);
-        for (Product p : products) {
-            System.out.println(p.toString());
-        }
+    void canGetCommentsByProductIdBetweenDate() {
+        productService.getCommentsByProductIdBetweenDate(
+                11L,
+                LocalDate.now().minusMonths(3L).toString(),
+                LocalDate.now().plusMonths(3L).toString());
+        //verify method calls in productService
+        verify(productRepository).findById(11L);
+        verify(productCommentRepository).findAllByProductAndCommentDateBetween(
+                returnProduct,
+                LocalDate.now().minusMonths(3L),
+                LocalDate.now().plusMonths(3L));
+        //testing if else statement in the product service
+        productService.getCommentsByProductIdBetweenDate(
+                11L,
+                "",
+                "");
+        verify(productCommentRepository).findByProduct(returnProduct);
+    }
+
+    @Test
+    void canGetExpiredProducts() {
+        productService.getExpiredProducts();
+        verify(productRepository).findAllExpired(LocalDate.now());
+
     }
 
     @Test
     void getNotExpiredProducts() {
-        List<Product> products = productService.getNotExpiredProducts();
-        Assertions.assertNotEquals(products.size(), 0);
-        for (Product p : products) {
-            System.out.println(p.toString());
-        }
+        productService.getNotExpiredProducts();
+        verify(productRepository).findAllNotExpired(LocalDate.now());
     }
 }
--- Case-1/src/test/java/com/example/case1/User/Service/UserServiceTest.java
+++ Case-1/src/test/java/com/example/case1/User/Service/UserServiceTest.java
@@ -1,38 +1,85 @@
 package com.example.case1.User.Service;
 
+import com.example.case1.Product.Entity.Product;
 import com.example.case1.ProductComment.Entity.ProductComment;
-import org.junit.jupiter.api.Assertions;
+import com.example.case1.ProductComment.Repository.ProductCommentRepository;
+import com.example.case1.User.Entity.User;
+import com.example.case1.User.Repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 class UserServiceTest {
-    @Autowired
+    private final User mockUser = new User();
+    @Mock
+    private ProductCommentRepository productCommentRepository;
+    @Mock
+    private UserRepository userRepository;
+
     private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        mockUser.setFirstName("ibrahim");
+        mockUser.setLastName("mohurlu");
+        mockUser.setEmail("him@mail.com");
+        mockUser.setPhoneNumber("123-9876-123");
+        mockUser.setId(5L);
+
+        Product returnProduct = new Product();
+        returnProduct.setId(11L);
+        returnProduct.setName("Expensive Mock Product!");
+        returnProduct.setPrice(469.89);
+        returnProduct.setExpDate(LocalDate.now().plusMonths(3L));
+
+        ProductComment returnComment = new ProductComment();
+        returnComment.setId(2L);
+        returnComment.setComment("Mock product is very expensive im not buying!");
+        returnComment.setCommentDate(LocalDate.now());
+        returnComment.setProduct(returnProduct);
+        returnComment.setUser(mockUser);
+
+        Mockito.when(userRepository.findById(5L)).thenReturn(Optional.of(mockUser));
+        Mockito.when(productCommentRepository.findByUser(mockUser)).thenReturn(List.of(returnComment));
+        Mockito.when(productCommentRepository.findAllByUserAndCommentDateBetween(
+                mockUser,
+                LocalDate.now().minusMonths(3L),
+                LocalDate.now().plusMonths(3L)
+        )).thenReturn(List.of(returnComment));
+
+        userService = new UserService(productCommentRepository, userRepository);
+    }
 
     @Test
     void getCommentsByUserId() {
-        List<ProductComment> comments = userService.getCommentsByUserId(3L,"","");
-        Assertions.assertNotEquals(comments.size(), 0);
-        for (ProductComment c : comments) {
-            System.out.println(c.toString());
-        }
+        userService.getCommentsByUserId(5L, "", "");
+        //verify method calls
+        verify(userRepository).findById(5L);
+        verify(productCommentRepository).findByUser(mockUser);
+
 
     }
 
     @Test
     void getCommentsByUserIdBetweenDates() {
-        List<ProductComment> comments = userService.getCommentsByUserId(
-                4L,
-                "2021-01-01",
-                "2022-12-31");
-        Assertions.assertNotEquals(comments.size(), 0);
-        for (ProductComment c : comments) {
-            System.out.println(c.toString());
-        }
+        userService.getCommentsByUserId(
+                5L,
+                LocalDate.now().minusMonths(3L).toString(),
+                LocalDate.now().plusMonths(3L).toString());
+        //verify method calls
+        verify(productCommentRepository).findAllByUserAndCommentDateBetween(
+                mockUser,
+                LocalDate.now().minusMonths(3L),
+                LocalDate.now().plusMonths(3L)
+        );
     }
 }
